b83757b815cbdea2a1339ceab4e5330bdc0b25ea
Nick Mason <nickmason372@yahoo.com>
2022-12-15 19:32:28 -0500

    added mutation functionality

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -54,7 +54,7 @@ const SearchBooks = () => {
   };
 
   // create function to handle saving a book to our database
-  const handleSaveBook = () => {
+  const HandleSaveBook = () => {
     const [bookState, setBookState] = useState({
       authors: [''],
       description: '',
@@ -64,10 +64,10 @@ const SearchBooks = () => {
       title: '',
 
     });
-    
+
     const [saveBook, {error}] = useMutation(SAVE_BOOK);
     // find the book in `searchedBooks` state by the matching id
-     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    //  const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
     // const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -78,11 +78,11 @@ const SearchBooks = () => {
 
     try {
       const {data} = saveBook({
-        variables:{...bookData}
+        variables:{...bookState},
       });
 
       // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+      // setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
     }
@@ -136,7 +136,7 @@ const SearchBooks = () => {
                     <Button
                       disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
                       className='btn-block btn-info'
-                      onClick={() => handleSaveBook(book.bookId)}>
+                      onClick={() => HandleSaveBook(book.bookId)}>
                       {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
                         ? 'This book has already been saved!'
                         : 'Save this Book!'}
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,19 +25,43 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-mutation saveBook ($userId: String! , $book: String!) {
-    saveBook($userId: _id, $book: book) {
-    _id
-    savedBooks
-}
-}`;
+    mutation saveBook($input: savedBook!) {
+    saveBook (input: $input)
+        {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
+
 
 export const REMOVE_BOOK = gql`
-mutation removeBook($book: String!) {
-    removeBook($book: book) {
-        _id
-        username
-        savedBooks
-        
-    }
-}`;
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId:$bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+}
+`;
